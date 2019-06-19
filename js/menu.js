558919a92dffa3a8a2806d94a2cacb4dcfbdbce3
fung.cheng <azusamine@azusamines-MacBook-Pro.local>
2019-06-19 10:12:06 +0900

    20190619-5

--- js/menu.js
+++ js/menu.js
@@ -9,10 +9,8 @@ $(function(){
         overlayMenuC.css("transition", "transform 0.5s ease-out");
         if(overlayMenuC.hasClass("overlay-toggle")){
             overlayMenuC.removeClass("overlay-toggle");
-            overlayItem.removeClass("animated fadeInLeftBig fast delay");
         }else{
             overlayMenuC.addClass("overlay-toggle");
-            overlayItem.addClass("animated fadeInLeftBig fast delay");
         }
     });
     toggleIcon.on("click", function(){
